b501492d61d78804d968a426614e9aba4863cefe
yoonseok ham <54228425+yoonseokham@users.noreply.github.com>
2025-06-22 16:07:13 +0900

    Time: 9 ms (12.68%), Space: 8.2 MB (25.35%) - LeetHub

---
+++ 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.go
@@ -0,0 +1,25 @@
+import "fmt"
+
+type Pair struct {
+    X, Y int
+}
+
+func findPairs(nums []int, k int) int {
+    kv_map := make(map[int]int)
+    seen := make(map[Pair]bool)
+    count := 0
+    for i:=0;i<len(nums);i++{
+        kv_map[nums[i]] += 1
+    }
+    for i:=0;i<len(nums);i++{
+        kv_map[nums[i]] -= 1
+        if kv_map[nums[i]+k] > 0 {
+            if !seen[Pair{nums[i], nums[i]+k}]{
+                count += 1
+                seen[Pair{nums[i], nums[i]+k}] = true
+            }
+        }
+        kv_map[nums[i]] += 1
+    }
+    return count
+}
