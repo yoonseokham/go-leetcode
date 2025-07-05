f384a509f444163a8ca6390682f7e105cb75f58a
yoonseok ham <54228425+yoonseokham@users.noreply.github.com>
2025-07-05 20:15:55 +0900

    Time: 0 ms (100%), Space: 17.9 MB (48.11%) - LeetHub

--- 2345-minimum-number-of-operations-to-convert-time/2345-minimum-number-of-operations-to-convert-time.py
+++ 2345-minimum-number-of-operations-to-convert-time/2345-minimum-number-of-operations-to-convert-time.py
@@ -2,7 +2,7 @@ class Solution:
     def convertTime(self, current: str, correct: str) -> int:
         answer = 0
         diff_min = Solution.getDiffMin(current, correct)
-        for i in [60, 15, 5, 1]:
+        for i in (60, 15, 5, 1):
             operation = diff_min//i
             answer += operation
             diff_min -= i*operation
@@ -10,6 +10,6 @@ class Solution:
 
     @staticmethod
     def getDiffMin(current: str, correct: str) -> int:
-        current_hour, current_min = [int(i) for i in current.split(":")]
-        correct_hour, correct_min = [int(i) for i in correct.split(":")]
+        current_hour, current_min = (int(i) for i in current.split(":"))
+        correct_hour, correct_min = (int(i) for i in correct.split(":"))
         return 60*(correct_hour - current_hour) + correct_min - current_min
