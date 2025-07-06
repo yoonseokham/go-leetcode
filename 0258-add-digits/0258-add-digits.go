bec1d86d30d94a795ef8c8e69cf01e97f4171016
yoonseok ham <54228425+yoonseokham@users.noreply.github.com>
2025-07-06 15:07:42 +0900

    Time: 0 ms (100%), Space: 4.2 MB (1.36%) - LeetHub

--- 0258-add-digits/0258-add-digits.go
+++ 0258-add-digits/0258-add-digits.go
@@ -5,10 +5,10 @@ import (
 
 func addDigits(num int) int {
     string_num := strconv.Itoa(num)
-    var sum_string_num int
     if len(string_num) == 1 {
         return num
     }
+    var sum_string_num int
     for i:=0;i<len(string_num);i++{
         temp, _ := strconv.Atoi(string(string_num[i]))
         sum_string_num += temp
