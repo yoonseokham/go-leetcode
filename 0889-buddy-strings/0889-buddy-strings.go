7cfc6ecda0b977dfee7ce5787b007f6213d455cd
yoonseok ham <54228425+yoonseokham@users.noreply.github.com>
2025-07-06 15:56:08 +0900

    Time: 0 ms (100%), Space: 4.4 MB (59.52%) - LeetHub

---
+++ 0889-buddy-strings/0889-buddy-strings.go
@@ -0,0 +1,23 @@
+func buddyStrings(s string, goal string) bool {
+    if len(s) != len(goal) {
+        return false
+    }
+    if s == goal {
+        s_dict := make(map[byte]bool)
+        for i:=0;i<len(s);i++{
+            s_dict[s[i]] = true
+        }
+        return len(s_dict) != len(s)
+    }
+    diffIndexs := make([]int,0,2)
+    for i:= 0;i<len(s);i++{
+        if s[i] != goal[i] {
+            diffIndexs = append(diffIndexs,i)
+        }
+        if len(diffIndexs) > 2 {return false}
+    }
+    if len(diffIndexs) == 1 {return false}
+    isOkay := (s[diffIndexs[0]] == goal[diffIndexs[1]] && s[diffIndexs[1]] == goal[diffIndexs[0]])
+    if !isOkay {return false}
+    return true
+}
