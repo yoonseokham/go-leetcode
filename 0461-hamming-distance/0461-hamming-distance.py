b09f6d8437af727d2be05862bf0d42637c1e33e9
yoonseok ham <54228425+yoonseokham@users.noreply.github.com>
2025-08-10 22:40:47 +0900

    Time: 4 ms (1.67%), Space: 17.7 MB (55.39%) - LeetHub

---
+++ 0461-hamming-distance/0461-hamming-distance.py
@@ -0,0 +1,18 @@
+import collections
+
+class Solution:
+    def getBinary(self, n :int) -> list[int]:
+        binary = [0 for i in range(33)]
+        i = 0
+        while n > 0:
+            binary[i] = n%2
+            n = n//2
+            i += 1
+        return binary
+
+    def hammingDistance(self, x: int, y: int) -> int:
+        answer = 0
+        for i,j in zip(self.getBinary(x), self.getBinary(y)):
+            if i != j:
+                answer += 1
+        return answer
