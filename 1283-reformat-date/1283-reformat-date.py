4854816a2addd7f7fc1aadd0be2de0e3786aba9e
yoonseok ham <54228425+yoonseokham@users.noreply.github.com>
2025-08-11 20:23:09 +0900

    Time: 0 ms (100%), Space: 17.7 MB (81.02%) - LeetHub

--- 1283-reformat-date/1283-reformat-date.py
+++ 1283-reformat-date/1283-reformat-date.py
@@ -1,24 +1,21 @@
 class Solution:
     MONTH_MAP = {
-        "Jan": "01",
-        "Feb": "02",
-        "Mar": "03",
-        "Apr": "04",
-        "May": "05",
-        "Jun": "06",
-        "Jul": "07",
-        "Aug": "08",
-        "Sep": "09",
+        "Jan": "1",
+        "Feb": "2",
+        "Mar": "3",
+        "Apr": "4",
+        "May": "5",
+        "Jun": "6",
+        "Jul": "7",
+        "Aug": "8",
+        "Sep": "9",
         "Oct": "10",
         "Nov": "11",
         "Dec": "12",
         }
-    def addZero(self, n: str) -> str:
-        return '0' + n if len(n) == 1 else n
     
     def reformatDate(self, date: str) -> str:
         day, month, year = list(date.split())
-        day = day[:-2]
-        month = self.MONTH_MAP[month]
-        return f"{year}-{month}-{self.addZero(day)}"
-        
+        day = '0' + day[:-2]
+        month = '0' + self.MONTH_MAP[month]
+        return f"{year}-{month[-2:]}-{day[-2:]}"
