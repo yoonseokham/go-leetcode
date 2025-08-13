487090a5d233c2f00865e5b1e45fc81d4a4d8536
yoonseok ham <54228425+yoonseokham@users.noreply.github.com>
2025-08-13 23:41:22 +0900

    Time: 45 ms (8.33%), Space: 15.4 MB (0%) - LeetHub

---
+++ 3252-count-the-number-of-incremovable-subarrays-i/3252-count-the-number-of-incremovable-subarrays-i.go
@@ -0,0 +1,31 @@
+import "fmt"
+
+func getRemovedNums(nums []int, start int, end int) []int {
+    copied := make([]int, len(nums))
+    copy(copied, nums)
+    new_nums := append(copied[:start], copied[end:]...)
+    return new_nums
+}
+
+func isIncreased(nums []int) bool {
+    temp := 0
+    for i:=0;i<len(nums);i++{
+        if temp >= nums[i] {
+            return false
+        }
+        temp = nums[i]
+    }
+    return true
+}
+
+func incremovableSubarrayCount(nums []int) int {
+    var answer int
+    for i:=0;i<=len(nums);i++{
+        for j:=i+1;j<=len(nums);j++{
+            if isIncreased(getRemovedNums(nums, i, j)) {
+                answer += 1
+            }     
+        }
+    }
+    return answer
+}
