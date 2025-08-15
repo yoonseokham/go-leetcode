e472124195048c360c5f9944bfb92abafc0e884e
yoonseok ham <54228425+yoonseokham@users.noreply.github.com>
2025-08-15 16:30:18 +0900

    Time: 7 ms (12.93%), Space: 9.9 MB (5.32%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.go
@@ -0,0 +1,12 @@
+func missingNumber(nums []int) int {
+    showed := make(map[int]bool)
+    for _, num := range nums {
+        showed[num] = true
+    }
+    for i:=0; i<len(nums);i++{
+        if !showed[i] {
+            return i
+        }
+    }
+    return len(nums)
+}
