e9d0f54dee5e895e66091ca41ba06b7b09360135
yoonseok ham <54228425+yoonseokham@users.noreply.github.com>
2025-08-15 15:59:29 +0900

    Time: 0 ms (100%), Space: 4.2 MB (58.99%) - LeetHub

---
+++ 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.go
@@ -0,0 +1,25 @@
+import "fmt"
+
+func maxFreqSum(s string) int {
+    v_max := 0
+    c_max := 0
+    vowels := map[byte]bool{
+        'a': true, 
+        'e': true,
+        'i': true,
+        'o': true,
+        'u':true,
+    }
+    counter := make(map[byte]int)
+    for i:=0 ;i<len(s);i++{
+        counter[s[i]]++
+    }
+    for k,v := range counter {
+        if vowels[k] {
+            v_max = max(v, v_max)
+        } else {
+            c_max = max(v, c_max)
+        }
+    }
+    return c_max + v_max
+}
