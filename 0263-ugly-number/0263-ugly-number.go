89e17fdd192b9bfe78a0b2954afcbd91b9fc8443
yoonseok ham <54228425+yoonseokham@users.noreply.github.com>
2025-08-16 14:28:50 +0900

    Time: 0 ms (100%), Space: 4.1 MB (8.33%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.go
+++ 0263-ugly-number/0263-ugly-number.go
@@ -1,18 +1,13 @@
 func isUgly(n int) bool {
-   for ;n>1;{
-    isDivided := false
-    for _, num := range []int{2,3,5}{
-        if n%num == 0 {
-            n = n/num
-            isDivided = true
+    isDivided := true
+    for ;isDivided && n > 1;{
+        isDivided = false
+        for _, num := range []int{2,3,5} {
+            if n%num == 0 {
+                n = n/num
+                isDivided = true
             }
         }
-    if !isDivided {
-        break
-        }
     }
-    if n != 1 {
-        return false
-    }
-    return true
+    return n == 1
 }
